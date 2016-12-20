ac6e35e55478f02a843515fdb4fff72f30bf2ab5
Caroline Chaudey <carolinechaudey@imac-de-olivier.digiplug.local>
2016-12-20 18:35:20 +0100

    Initial Commit

---
+++ EasyCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyCalc
+//
+//  Created by Caroline Chaudey on 20/12/2016.
+//  Copyright © 2016 esgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyCalc
+//
+//  Created by Caroline Chaudey on 20/12/2016.
+//  Copyright © 2016 esgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
