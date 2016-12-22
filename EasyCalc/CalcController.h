dae45d83deba2428712dbe00502d647af055428c
Caroline Chaudey <carolinechaudey@imac-de-olivier.digiplug.local>
2016-12-22 18:10:40 +0100

    init

---
+++ EasyCalc/CalcController.h
@@ -0,0 +1,13 @@
+//
+//  CalcController.h
+//  EasyCalc
+//
+//  Created by Caroline Chaudey on 21/12/2016.
+//  Copyright © 2016 esgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalcController : UIViewController
+
+@end
---
+++ EasyCalc/CalcController.m
@@ -0,0 +1,37 @@
+//
+//  CalcController.m
+//  EasyCalc
+//
+//  Created by Caroline Chaudey on 21/12/2016.
+//  Copyright © 2016 esgi. All rights reserved.
+//
+
+#import "CalcController.h"
+
+@interface CalcController ()
+
+@end
+
+@implementation CalcController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
